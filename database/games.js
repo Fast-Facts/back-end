4994e0842eea06d06f90663d4a59c7113f1e28f2
Anthony Venturini <anthonydventurini@gmail.com>
2020-04-04 14:05:37 -0700

    💄 Remove console.logs

--- database/games.js
+++ database/games.js
@@ -13,18 +13,15 @@ function get() {
 
 function insert(game) {
   const newGame = { user: game.user, name: game.name };
-//   console.log(newGame);
   return db("games")
     .insert(newGame)
     .returning("id")
     .then((ids) => {
-      console.log(ids, "GAME SRTHSRTHSRTHSRTHSRTHSRHTID")
       return ids;
     }); 
 }
 
 function add(game, id ) {
-  console.log(game, id, "here");
   const question = game.questions.map((question) => {
       return {question: question[0], answer: question[1], games_id: id}})
     return db("questions")
@@ -34,7 +31,6 @@ function add(game, id ) {
   };
 
 function getQuestions(id){
-  console.log(id)
   return db("questions")
     .where("questions.games_id", id)
 }
--- index.js
+++ index.js
@@ -13,14 +13,11 @@ const port = process.env.PORT
 
 app.post("/", async (req,res) => {
 
-    console.log(req.body)
 
     try{
         const game = req.body;
-        console.log(game)
 
         const newGame = await db.insert(game)
-        console.log(newGame)
         const newQuestions = await db.add(game, newGame[0])
         // console.log(game)
         res.status(201).json(newGame)
@@ -28,7 +25,6 @@ app.post("/", async (req,res) => {
 
     catch(error){
         res.status(500).json({message: "Error creating game"})
-        console.log(error)
     }
 
 })
@@ -37,14 +33,12 @@ app.get("/", async (req, res) => {
     
     try{
         const games = await db.get()
-        console.log("hello")
         if(!games) {
             return res.status(404).json({message: "No games were found"})
         }
         res.status(200).json(games)
     }
     catch(err){
-        console.log(err)
         res.status(500).json({message: "Error retrieving games"})
     }
 }
@@ -52,11 +46,9 @@ app.get("/", async (req, res) => {
 
 app.get("/games/:id", async (req, res)=> {
 
-    console.log(req.params.id)
 
     const id = req.params.id
 
-    console.log(id)
     
     try{
         games = await db.getQuestions(id)
